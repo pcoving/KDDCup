1ae3fac655438f1b13d874a1cf4b0ddc8b521108
pcoving <pmcovington@gmail.com>
2013-04-20 16:15:00 -0700

    trying to reproduce the kaggle benchmark

---
+++ features.py
@@ -0,0 +1,118 @@
+import csv
+import cPickle
+import numpy as np
+from collections import defaultdict
+
+class Author():
+    def __init__(self):
+        self.papers = []
+        
+class Paper():
+    def __init__(self):
+        self.authors = []
+        self.year = None
+        self.journalid = None
+        self.conferenceid = None
+
+def loadAuthorsAndPapers(path='DataRev2/'):
+    
+    authors = {}
+    papers = {}
+    with open(path + 'PaperAuthor.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        reader.next() # skip header
+        for paperid, authorid, name, affiliation in reader:
+            paperid, authorid = int(paperid), int(authorid)
+            if paperid not in papers:
+                papers[paperid] = Paper()
+            papers[paperid].authors.append(int(authorid))
+            
+            if authorid not in authors:
+                authors[authorid] = Author()
+            authors[authorid].papers.append(int(paperid))
+
+    notfound = 0
+    with open(path + 'Paper.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        reader.next() # skip header
+        for paperid, title, year, conferenceid, journalid, keyword in reader:
+            paperid = int(paperid)
+            try:
+                papers[paperid].year = int(year)
+                papers[paperid].conferenceid = int(conferenceid)
+                papers[paperid].journalid = int(journalid)
+            except KeyError:
+                notfound += 1
+
+    print 'unable to find ' + str(notfound) + ' papers '
+
+    return authors, papers
+
+def buildFeatures(authors, papers, path='DataRev2/'):
+    # recreates features from Kaggle benchmark without SQL    
+
+    labels = []
+    features = []
+    
+    
+    # build features for training set...
+    '''
+    so far:
+    Number of Authors on Paper
+    Year of Paper
+    Number of Papers by Author
+    Number of Author's Papers in Journal
+    Number of Author's Papers in Conference
+    '''
+    with open('DataRev2/Train.csv') as csvfile:
+        reader = csv.reader(csvfile)
+        reader.next() # skip header
+        for authorid, confirmedids, deletedids in reader:
+            authorid = int(authorid)
+            confirmedids = [int(id) for id in confirmedids.split(' ')]
+            deletedids = [int(id) for id in deletedids.split(' ')]
+
+            myfeatures, mylabels = [], []
+            for cid in confirmedids:
+                mylabels.append(1)  # 1 = confirmed
+                myfeatures.append(generateFeatures(cid, authorid, papers, authors))
+            for did in deletedids:
+                mylabels.append(0)  # 0 = deleted
+                myfeatures.append(generateFeatures(did, authorid, papers, authors))
+            
+            features.append(myfeatures)    
+            labels.append(mylabels)
+    
+    return labels, features
+
+def saveFeatures(labels, features, filename='train_features.p'):
+    print 'saving features...'
+    cPickle.dump([labels, features], open(filename,'wb'))
+
+    
+def generateFeatures(paperid, authorid, papers, authors):
+    nauthors = len(papers[paperid].authors)
+    npapers = len(authors[authorid].papers)
+    year = papers[paperid].year
+    if papers[paperid].conferenceid > 0:
+        nconference = 0
+        for pid in authors[authorid].papers:
+            if papers[pid].conferenceid == papers[paperid].conferenceid:
+                nconference += 1
+    else:
+        nconference = -1  # indicates no conference info in data
+    if papers[paperid].journalid > 0:
+        njournal = 0
+        for pid in authors[authorid].papers:
+            if papers[pid].journalid == papers[paperid].journalid:
+                njournal += 1
+    else:
+        njournal = -1  # indicates no journal info in data
+    
+    features = [npapers, nauthors, year, nconference, njournal]
+    return features
+
+if __name__ == '__main__':
+    authors, papers = loadAuthorsAndPapers()
+    labels, features = buildFeatures(authors, papers)
+    saveFeatures(labels, features)
---
+++ train.py
@@ -0,0 +1,59 @@
+import numpy as np
+import cPickle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import ShuffleSplit
+    
+def shuffleCrossValidation(labels, features, classifier, 
+                           n_iter=3, test_size=0.25, random_state=0):
+    
+    ss = ShuffleSplit(len(labels), n_iter=n_iter, 
+                      test_size=test_size, random_state=random_state)
+    
+    score = []
+    for train_authors, test_authors in ss:
+        myscore = 0.0
+        X_train = [val for idx in train_authors for val in features[idx]]
+        Y_train = [val for idx in train_authors for val in labels[idx]]
+        
+        classifier = classifier.fit(X_train, Y_train)
+        
+        # predict everything at once
+        X_test = [val for idx in test_authors for val in features[idx]]
+        P_test = classifier.predict_proba(X_test)[:,1]
+        
+        flatidx = 0
+        for idx in test_authors:
+            npapers = len(labels[idx])
+            ranking = P_test[flatidx:flatidx+npapers].argsort()[::-1]
+            flatidx += npapers
+            
+            ranked_labels = [labels[idx][rank] for rank in ranking]
+            myscore += scoreAuthor(ranked_labels)
+            
+        score.append(myscore/len(test_authors))
+    
+    score = np.array(score)
+    print 'score mean, std:', score.mean(), score.std()
+            
+def scoreAuthor(ranked_labels):
+    # computes average precision for labels of a ranked list of an author's papers
+    
+    score = 0.0
+    confirmedCount = 0
+    for idx, label in enumerate(ranked_labels):
+        if label is 1:
+            confirmedCount += 1
+            score += float(confirmedCount)/float(idx+1)            
+    score /= sum(ranked_labels)
+
+    return score
+    
+if __name__ == '__main__':
+    classifier = RandomForestClassifier(n_estimators=100, 
+                                        n_jobs=-1,
+                                        min_samples_split=10,
+                                        random_state=0)
+                                        
+    labels, features = cPickle.load(open('train_features.p', 'rb'))
+    
+    shuffleCrossValidation(labels, features, classifier)
