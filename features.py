530ccd8825d7a63badb39d1d03098e8fa2de6d9f
Dan Frank <danfrankj@gmail.com>
2013-05-14 21:47:32 -0700

    tried to understand dataflow; managed to get rid of excess whitespace and fix a very small bug

--- features.py
+++ features.py
@@ -25,7 +25,7 @@ class Venue():
 
 def loadAuthorsPapers(path='dataRev2/'):
     print 'loading authors and papers...'
-    
+
     authors = {}
     papers = {}
     with open(path + 'PaperAuthor.csv') as csvfile:
@@ -37,12 +37,12 @@ def loadAuthorsPapers(path='dataRev2/'):
                 papers[paperid] = Paper()
             papers[paperid].authors.append(authorid)
             # no need for this yet..
-            papers[paperid].affiliation = affiliation 
-            
+            papers[paperid].affiliation = affiliation
+
             if authorid not in authors:
                 authors[authorid] = Author()
             authors[authorid].papers.append(paperid)
-            
+
     notfound = 0
     with open(path + 'Paper.csv') as csvfile:
         reader = csv.reader(csvfile)
@@ -58,7 +58,7 @@ def loadAuthorsPapers(path='dataRev2/'):
                     # some papers are in both a conference and a journal?
                     papers[paperid].venueid = conferenceid
                 # no need for this yet...
-                papers[paperid].title = title 
+                papers[paperid].title = title
             except KeyError:
                 notfound += 1
 
@@ -68,12 +68,12 @@ def loadAuthorsPapers(path='dataRev2/'):
 
     # some (~500) papers don't have author information? could be an artifact of how the data was generated, but only a tiny fraction...
     #print 'unable to find ' + str(notfound) + ' papers'
-    
+
     print 'done.'
     return authors, papers
 
 def loadVenues(authors, papers):
-    
+
     venues = {}
     for pid in papers.keys():
         vid = papers[pid].venueid
@@ -81,7 +81,7 @@ def loadVenues(authors, papers):
             if vid not in venues:
                 venues[vid] = Venue()
             venues[vid].papers.append(pid)
-    
+
     for venue in venues.values():
         print venue.papers
         sys.stdin.read(1)
@@ -89,7 +89,7 @@ def loadVenues(authors, papers):
     return venues
 
 def csvGenerator(mode, path='dataRev2/'):
-    
+
     if mode == 'train':
         with open(path + 'Train.csv') as csvfile:
             reader = csv.reader(csvfile)
@@ -117,10 +117,10 @@ def csvGenerator(mode, path='dataRev2/'):
 def saveFeature(feature, name, mode):
     filename = name + '.' + mode
     print 'saving feature to', filename, '...'
-    cPickle.dump(feature, open(filename,'wb'))
+    cPickle.dump(feature, open(filename, 'wb'))
 
 def labels(mode='train', path='dataRev2/'):
-    
+
     labels = []
     if mode == 'train':
         with open(path + 'Train.csv') as csvfile:
@@ -131,7 +131,7 @@ def labels(mode='train', path='dataRev2/'):
                 authorid = int(authorid)
                 confirmedids = [int(id) for id in confirmedids.split(' ')]
                 deletedids = [int(id) for id in deletedids.split(' ')]
-                
+
                 for cid in confirmedids:
                     mylabels.append(1)  # 1 = confirmed
                 for did in deletedids:
@@ -147,8 +147,8 @@ def labels(mode='train', path='dataRev2/'):
     else:
         print 'mode must be "train" or "test"'
         raise ValueError
-            
-    saveFeature(labels, name='labels', mode=mode) 
+
+    saveFeature(labels, name='labels', mode=mode)
 
 def nauthors(papers, authors, mode='train', path='dataRev2/'):
     '''
@@ -161,7 +161,7 @@ def nauthors(papers, authors, mode='train', path='dataRev2/'):
     for authorid, paperids in csvGenerator(mode=mode, path=path):
         features.append([len(papers[pid].authors) for pid in paperids])
 
-    saveFeature(features, name='nauthors', mode=mode) 
+    saveFeature(features, name='nauthors', mode=mode)
 
 def npapers(papers, authors, mode='train', path='dataRev2/'):
     '''
@@ -174,7 +174,7 @@ def npapers(papers, authors, mode='train', path='dataRev2/'):
     for authorid, paperids in csvGenerator(mode=mode, path=path):
         features.append([len(authors[authorid].papers) for pid in paperids])
 
-    saveFeature(features, name='npapers', mode=mode) 
+    saveFeature(features, name='npapers', mode=mode)
 
 def year(papers, authors, mode='train', path='dataRev2/'):
     '''
@@ -187,15 +187,15 @@ def year(papers, authors, mode='train', path='dataRev2/'):
     for authorid, paperids in csvGenerator(mode=mode):
         features.append([papers[pid].year for pid in paperids])
 
-    saveFeature(features, name='year', mode=mode) 
+    saveFeature(features, name='year', mode=mode)
 
 def nsamevenue(papers, authors, mode='train', path='dataRev2/'):
     '''
     Number of times author has published at venue
     '''
-    
+
     print 'generating nsamevenue feature...'
-    
+
     features = []
     for authorid, paperids in csvGenerator(mode=mode, path=path):
         myfeatures = []
@@ -207,13 +207,13 @@ def nsamevenue(papers, authors, mode='train', path='dataRev2/'):
 
         features.append(myfeatures)
 
-    saveFeature(features, name='nsamevenue', mode=mode) 
+    saveFeature(features, name='nsamevenue', mode=mode)
 
 def nattrib(papers, authors, mode='train', path='dataRev2/'):
     '''
     Number of times paper has been attributed to author
     '''
-    
+
     print 'generating nattrib feature...'
 
     features = []
@@ -222,16 +222,16 @@ def nattrib(papers, authors, mode='train', path='dataRev2/'):
         for pid in paperids:
             myfeatures.append(authors[authorid].papers.count(pid))
         features.append(myfeatures)
-        
-    saveFeature(features, name='nattrib', mode=mode) 
+
+    saveFeature(features, name='nattrib', mode=mode)
 
 def paperrank(papers, authors, mode='train', path='dataRev2/', beta=0.8, nwalks=50):
     '''
     Personalized page rank
     '''
-    
+
     print 'generating paperrank feature...'
-    
+
     features = []
     for authorid, paperids in csvGenerator(mode=mode, path=path):
         for pid in paperids:
@@ -247,18 +247,18 @@ def paperrank(papers, authors, mode='train', path='dataRev2/', beta=0.8, nwalks=
                             random_aid = random.choice(papers[current_pid].authors)
                         current_pid = random.choice(authors[random_aid].papers)
                         papers[current_pid].paperrank += 1
-                        
+
         features.append([papers[pid].paperrank for pid in paperids])
-        
-    saveFeature(features, name='paperrank', mode=mode) 
+
+    saveFeature(features, name='paperrank', mode=mode)
 
 def globalpaperrank(papers, authors, mode='train', path='dataRev2/'):
     '''
     On the undirected paper-author graph, the page rank is simply the degree
     '''
-    
+
     print 'generating globalpaperrank feature...'
-    
+
     features = []
     for authorid, paperids in csvGenerator(mode=mode, path=path):
         myfeatures = []
@@ -268,18 +268,18 @@ def globalpaperrank(papers, authors, mode='train', path='dataRev2/'):
                 if aid != authorid:
                     for pid in authors[aid].papers:
                         globalpaperrank += 1
-                            
+
             myfeatures.append(globalpaperrank)
         features.append(myfeatures)
-    
-    saveFeature(features, name='globalpaperrank', mode=mode) 
+
+    saveFeature(features, name='globalpaperrank', mode=mode)
 
 
 def ncoauthor(papers, authors, mode='train', path='dataRev2/'):
     '''
     Number of times author has published with coauthors on paper
     '''
-    
+
     print 'generating ncoauthor feature...'
 
     features = []
@@ -294,17 +294,17 @@ def ncoauthor(papers, authors, mode='train', path='dataRev2/'):
                             ncoauthor += 1
             myfeatures.append(ncoauthor)
         features.append(myfeatures)
-    
-    saveFeature(features, name='ncoauthor', mode=mode) 
-    
+
+    saveFeature(features, name='ncoauthor', mode=mode)
+
 if __name__ == '__main__':
-    
+
     labels(mode='train')
-    
+
     authors, papers = loadAuthorsPapers()
     #venues = loadVenues(authors, papers)
-    
-    
+
+
     nauthors(papers, authors, mode='train')
     npapers(papers, authors, mode='train')
     year(papers, authors, mode='train')
@@ -313,4 +313,4 @@ if __name__ == '__main__':
     globalpaperrank(papers, authors, mode='train')
     paperrank(papers, authors, mode='train')
     ncoauthor(papers, authors, mode='train')
-    
+
--- train.py
+++ train.py
@@ -2,25 +2,22 @@ import numpy as np
 import cPickle
 import csv
 import itertools
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn.ensemble import RandomForestClassifier
 #from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.cross_validation import ShuffleSplit
-from rankboost import BipartiteRankBoost
+#from rankboost import BipartiteRankBoost
 import random
-import sys
-from sklearn.externals.joblib import Parallel, delayed
-import time
 
 def crossValidation(labels, features, classifier, train_authors, test_authors):
 
     X_train = [val for idx in train_authors for val in features[idx]]
     y_train = [val for idx in train_authors for val in labels[idx]]
     classifier = classifier.fit(X_train, y_train)
-        
+
     X_test = [val for idx in test_authors for val in features[idx]]
     P_test = classifier.predict_proba(X_test)[:,1]
-    
+
     myscore = 0.0
     flatidx = 0
     for idx in test_authors:
@@ -31,20 +28,20 @@ def crossValidation(labels, features, classifier, train_authors, test_authors):
         flatidx += npapers
 
         ranked_labels = [mylabels[rank] for rank in ranking]
-        
+
         myscore += scoreAuthor(ranked_labels)
-            
+
     return myscore/len(test_authors)
-    
-def shuffleCrossValidation(labels, features, classifier, 
+
+def shuffleCrossValidation(labels, features, classifier,
                            n_iter=5, test_size=0.25, random_state=1,
                            verbose=0, pairwise=False):
-    
-    ss = ShuffleSplit(len(labels), n_iter=n_iter, 
+
+    ss = ShuffleSplit(len(labels), n_iter=n_iter,
                       test_size=test_size, random_state=random_state)
-    
+
     #scores = Parallel(n_jobs=-1)(delayed(crossValidation)(labels, features, classifier, train_authors, test_authors) for train_authors, test_authors in ss)
-    
+
     score = []
     for train_authors, test_authors in ss:
 
@@ -60,25 +57,25 @@ def shuffleCrossValidation(labels, features, classifier,
                 return int(classifier.predict(xtrans)[0])
 
         classifier = classifier.fit(X_train, y_train)
-        
+
         if not pairwise:
             # predict everything at once
             X_test = [val for idx in test_authors for val in features[idx]]
             P_test = classifier.predict_proba(X_test)[:,1]
-                
+
         myscore = 0.0
         flatidx = 0
         for idx in test_authors:
             myfeatures, mylabels = features[idx], labels[idx]
 
-            if pairwise:                
+            if pairwise:
                 c = zip(myfeatures, mylabels)
                 random.shuffle(c)
                 myfeatures = [e[0] for e in c]
                 mylabels = [e[1] for e in c]
-                        
-                ranking = sorted(range(len(myfeatures)), 
-                                 key=myfeatures.__getitem__, 
+
+                ranking = sorted(range(len(myfeatures)),
+                                 key=myfeatures.__getitem__,
                                  cmp=classifierComp)
             else:
                 npapers = len(mylabels)
@@ -88,21 +85,21 @@ def shuffleCrossValidation(labels, features, classifier,
             ranked_labels = [mylabels[rank] for rank in ranking]
 
             myscore += scoreAuthor(ranked_labels)
-            
+
         score.append(myscore/len(test_authors)) # MAP
         if verbose > 0:
             print 'score:', myscore/len(test_authors)
 
-    scores = np.array(scores)
-    print 'score mean, std:', scores.mean(), scores.std()
-            
-def trainAndPredict(trainlabels, trainfeatures, 
+    score = np.array(score)
+    print 'score mean, std:', score.mean(), score.std()
+
+def trainAndPredict(trainlabels, trainfeatures,
                     testlabels, testfeatures, classifier, pairwise=False):
-        
+
     if not pairwise:
-        X_train = [paperfeature for authorfeatures in trainfeatures 
+        X_train = [paperfeature for authorfeatures in trainfeatures
                    for paperfeature in authorfeatures]
-        y_train = [paperlabel for authorlabels in trainlabels 
+        y_train = [paperlabel for authorlabels in trainlabels
                    for paperlabel in authorlabels]
     else:
         X_train = [authorfeatures for authorfeatures in trainfeatures]
@@ -115,26 +112,26 @@ def trainAndPredict(trainlabels, trainfeatures,
     classifier.fit(X_train, y_train)
 
     if not pairwise:
-        X_test = [paperfeature for authorfeatures in testfeatures 
+        X_test = [paperfeature for authorfeatures in testfeatures
                   for paperfeature in authorfeatures]
         P_test = classifier.predict_proba(X_test)[:,1]
-        
+
     flatidx = 0
     ranked_papers = []
     for idx, labels in enumerate(testlabels):
         author, papers = labels
 
         if pairwise:
-            ranking = sorted(range(len(testfeatures[idx])), 
-                             key=testfeatures[idx].__getitem__, 
+            ranking = sorted(range(len(testfeatures[idx])),
+                             key=testfeatures[idx].__getitem__,
                              cmp=classifierComp)
         else:
             npapers = len(papers)
             ranking = P_test[flatidx:flatidx+npapers].argsort()[::-1]
             flatidx += npapers
-        
+
         ranked_papers.append([author, [papers[rank] for rank in ranking]])
-        
+
     with open('submission.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
         for author, papers in ranked_papers:
@@ -145,7 +142,7 @@ def pairwise_transform(labels, features):
     new_features = []
     new_labels = []
     for label, feature in zip(labels, features):
-        
+
         nconfirmed = sum(label)
         for iconf, idel in itertools.product(range(nconfirmed), range(nconfirmed, len(label))):
             assert label[iconf] == 1 and label[idel] == 0
@@ -153,7 +150,7 @@ def pairwise_transform(labels, features):
             new_labels.append(1)
             new_features.append(feature[idel] + feature[iconf] + [feat2 - feat1 for (feat1, feat2) in zip(feature[iconf], feature[idel])])
             new_labels.append(-1)
-            
+
     return new_labels, new_features
 
 def loadFeatures(namelist, mode):
@@ -164,7 +161,7 @@ def loadFeatures(namelist, mode):
     features = []
     for feats in zip(*featurelist):
         features.append([list(tup) for tup in zip(*feats)])
-        
+
     return features
 
 def scoreAuthor(ranked_labels):
@@ -175,37 +172,37 @@ def scoreAuthor(ranked_labels):
     for idx, label in enumerate(ranked_labels):
         if label is 1:
             confirmedCount += 1
-            score += float(confirmedCount)/float(idx+1)            
+            score += float(confirmedCount)/float(idx+1)
     score /= ranked_labels.count(1)
 
     return score
 
 if __name__ == '__main__':
-    #classifier = RandomForestClassifier(n_estimators=100, 
+    #classifier = RandomForestClassifier(n_estimators=100,
     #                                    n_jobs=-1,
     #                                    min_samples_split=10,
     #                                    random_state=1)
-    
+
     #classifier = AdaBoostClassifier(n_estimators=200)
     #classifier = BipartiteRankBoost(n_estimators=50, verbose=1)
 
     feature_list = ['nauthors', 'npapers', 'year', 'nsamevenue', 'nattrib', 'paperrank', 'globalpaperrank', 'ncoauthor']
 
     trainfeatures = loadFeatures(feature_list, mode='train')
-    trainlabels = cPickle.load(open('labels.train', 'rb')) 
+    trainlabels = cPickle.load(open('labels.train', 'rb'))
 
-    classifier = GradientBoostingClassifier(n_estimators=200, 
-                                            #subsample=subsample, 
+    classifier = GradientBoostingClassifier(n_estimators=200,
+                                            #subsample=subsample,
                                             #learning_rate=learning_rate,
-                                            #min_samples_split=2, 
+                                            #min_samples_split=2,
                                             #min_samples_leaf=1,
                                             #max_depth=max_depth,
                                             random_state=1,
                                             verbose=0)
-    
+
     shuffleCrossValidation(trainlabels, trainfeatures, classifier, n_iter=5, verbose=2, pairwise=False)
-    
+
     #trainAndPredict(trainlabels, trainfeatures, testlabels, testfeatures, classifier, pairwise=True)
 
 
-    
+
